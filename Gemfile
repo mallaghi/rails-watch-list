37ff512f96ec8269ae2188f1bb38eac1e321ddc7
maram allaghi <writtenbymaram@gmail.com>
2023-05-09 19:59:58 +0100

    got all the backend logic finished, need to start with front end

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "sassc-rails"
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
+gem "rest-client"
+gem 'faker'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:index, :new, :create]
+    resources :movies, only: [:index]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
